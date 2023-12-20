0a6919cdad9495c4f2f3c61b6b23d702584a230e
Ololade Sanya <ololadetimileyin3@gmail.com>
2023-12-20 15:19:44 +0100

    initialized npm and installed some dependencies

--- lesson3/index.js
+++ lesson3/index.js
@@ -1,3 +1,8 @@
-const { format } = require('date-fns')
+const { format } = require('date-fns');
+const {v4: uuid} = require('uuid');
 
 console.log(format(new Date(),  'yyyyMMdd\tHH:mm:ss'))
+
+console.log(uuid())
+
+console.log()
