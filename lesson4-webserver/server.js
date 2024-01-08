1584352b58dbe180608b5742f7ddc20dfda33851
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-08 13:14:20 +0100

    learning express and node js

--- lesson4-webserver/server.js
+++ lesson4-webserver/server.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const app = express();
-const logEvents = require('./middleware/logEvents')
+const {logger} = require('./middleware/logEvents')
 const path = require('path');
 const PORT = process.env.PORT || 3500;
 
-app.use((req, res, next ) => {
-    logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
-    console.log(`${req.method} ${req.path}`);
-    next();
-});
+app.use( logger);
 
 app.use(express.urlencoded({ extended: false }))
 
