fdcc366fe2590a37894515891d4cd88f3774f826
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-10 00:07:50 +0100

    doing node and express is fun learning here

--- lesson4-webserver/routes/api/employees.js
+++ lesson4-webserver/routes/api/employees.js
@@ -5,4 +5,34 @@ const path = require('path');
 const data = {};
 data.employees = require('../../data/employees.json')
 
+router.route('/')
+   .get((req, res) => {
+       res.json(data.employees);
+   })
+   .post((req, res) => {
+       res.json({
+        "firstname": req.body.firstname,
+        "lastname": req.body.lastname
+       });
+   })
+   .put((req, res) => {
+       res.json({
+        "firstname": req.body.firstname,
+        "lastname": req.body.lastname
+       })
+   })
+   .delete((req, res) => {
+       res.json({
+        "id": req.body.id
+       })
+   })
+
+router.route('/:id')
+    .get((req, res) => {
+        res.json({ 
+            "id": req.params.id
+        })
+    })
+
+module.exports = router;
 
--- lesson4-webserver/server.js
+++ lesson4-webserver/server.js
@@ -32,12 +32,7 @@ app.use('/subdir', express.static(path.join(__dirname, "/public",)));
 
 app.use('/', require('./routes/root'));
 app.use('/subdir', require('./routes/subdir'));
-
-
-
-
-
-
+app.use('/employees', require('./routes/api/employees'));
 
 app.all('*', (req, res) => {
     res.status(404);
