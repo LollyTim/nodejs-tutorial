d699e4708cc31753af16a7dcc438033a5ed807b1
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-09 23:05:09 +0100

    doing node and express is fun learning here

--- lesson4-webserver/server.js
+++ lesson4-webserver/server.js
@@ -32,39 +32,11 @@ app.use('/subdir', express.static(path.join(__dirname, "/public",)))
 
 app.use('/subdir', require('./routes/subdir'))
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "index.html"))
-})
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "new-page.html"))
-})
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, "new-page.html")
-})
-
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log("Attempten to load hello.html");
-    next();
-}, (req, res) => {
-    res.send('Hello World!!');
-});
 
 
-const one = (req, res, next) => {
-    console.log("one");
-    next();
-}
-const two = (req, res, next) => {
-    console.log("two");
-    next();
-}
 
-const three = (req, res) => {
-    console.log("three"); b
-    res.send('Finished');
-};
 
-app.get('/chain(.html)?', [one, two, three]);
+
 
 app.all('*', (req, res) => {
     res.status(404);
