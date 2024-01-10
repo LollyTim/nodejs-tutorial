ca708898ec932b522efc9b9af041ecd13fb966f1
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-10 16:12:08 +0100

    doing  hard coding in node js and expressjs

---
+++ lesson4-webserver/controllers/authController.js
@@ -0,0 +1,23 @@
+const usersDB = {
+    users: require('../model/users.json'),
+    setUsers: function (data) {this.users = data}
+}
+
+const bcrypt = require('bcrypt');
+
+const handleLogin = async (req, res) => {
+    const { user, pwd} = req.body;
+    if (!user || !pwd) return res.status(400).json({'message': 'user and password are required. '})
+    const foundUser = usersDB.users.find(person => preson.username === user)
+    if (!foundUser) return res.sendStatus(401) //Unautorized
+    //evaluate password 
+    const match = await bcrypt.compare(pwd, foundUser.password)
+    if (match) {
+        res.json({"success": `User ${user} is logged in.`});
+    }else {
+        res.sendStatus(401);
+    }
+}
+
+
+module.exports = { handleLogin }
