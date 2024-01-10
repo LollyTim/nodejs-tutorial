5c67aebc071b587c6b4d7c645fc95220dc49786f
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-10 16:25:47 +0100

    doing  hard coding in node js and expressjs

---
+++ lesson4-webserver/routes/auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const registerController = require('../controllers/authController');
+
+
+router.post('/', registerController.handleLogin);
+
+
+module.exports = router;
