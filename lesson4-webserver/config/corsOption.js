937210abff499ef2c38db498a67b8c7ef01a0ad6
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-11 17:52:11 +0100

    doing hard things

--- lesson4-webserver/config/corsOption.js
+++ lesson4-webserver/config/corsOption.js
@@ -1,21 +1,14 @@
-const whitelist = [
-    'https://www.google.com', 
-    'https://www.youtube.com', 
-    'https://expressjs.com',
-    'http://127.0.0.1:5500', 
-    'http://localhost:3500'
-]
+const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin){
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
-        }else {
-            callback(new Error("Not Allowed By CORS"));
+        } else {
+            callback(new Error('Not allowed by CORS'));
         }
     },
     optionsSuccessStatus: 200
 }
-
 
 module.exports = corsOptions;
