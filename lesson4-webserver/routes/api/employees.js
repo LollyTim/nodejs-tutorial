d01353a0de596d66e3e5e9849743c6ac37ed1e87
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-11 10:14:44 +0100

    doing nodejs cool

--- lesson4-webserver/routes/api/employees.js
+++ lesson4-webserver/routes/api/employees.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const employeesCOntroller = require('../../controllers/employeesController')
-
+const verifyJWT = require('../../middleware/verifyJWT')
 router.route('/')
    .get(employeesCOntroller.getAllEmployees)
    .post(employeesCOntroller.createNewEmployee)
