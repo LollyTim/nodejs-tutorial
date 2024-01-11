96cb67fcea960379dead5084a20f5d14e3e1be7f
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-11 17:52:52 +0100

    doing hard things

--- lesson4-webserver/server.js
+++ lesson4-webserver/server.js
@@ -7,7 +7,7 @@ const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const verifyJWT = require('./middleware/verifyJWT');
 const cookieParser = require('cookie-parser');
-// const credentials = require('./middleware/credentials');
+const credentials = require('./middleware/credentials');
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
