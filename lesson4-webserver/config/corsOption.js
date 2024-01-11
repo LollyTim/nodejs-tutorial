4a157b2a57f3161100388bba83d419bc2fc95d65
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-11 18:08:04 +0100

    doing hard things

--- lesson4-webserver/config/corsOption.js
+++ lesson4-webserver/config/corsOption.js
@@ -1,14 +1,11 @@
-const allowedOrigins = require('./allowedOrigins');
+const allowedOrigins = require('../config/allowedOrigins');
 
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionsSuccessStatus: 200
+const credentials = (req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Credentials', true);
+    }
+    next();
 }
 
-module.exports = corsOptions;
+module.exports = credentials
