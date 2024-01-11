6a5fd739757e5baffcb7412e5585c94e23e44971
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-11 17:51:44 +0100

    doing hard things

--- lesson4-webserver/server.js
+++ lesson4-webserver/server.js
@@ -36,8 +36,8 @@ app.use('/', express.static(path.join(__dirname, '/public')));
 app.use('/', require('./routes/root'));
 app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
-// app.use('/refresh', require('./routes/refresh'));
-// app.use('/logout', require('./routes/logout'));
+app.use('/refresh', require('./routes/refresh'));
+app.use('/logout', require('./routes/logout'));
 
 app.use(verifyJWT);
 app.use('/employees', require('./routes/api/employees'));
