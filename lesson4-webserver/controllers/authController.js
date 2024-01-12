6579e17c7356bf96d82b4a2776dafcdc4779f334
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-13 00:18:39 +0100

    doing what i love to do i think im loving backed more

--- lesson4-webserver/controllers/authController.js
+++ lesson4-webserver/controllers/authController.js
@@ -17,9 +17,15 @@ const handleLogin = async (req, res) => {
     // evaluate password 
     const match = await bcrypt.compare(pwd, foundUser.password);
     if (match) {
+        const roles = Object.values(foundUser.roles)
         // create JWTs
         const accessToken = jwt.sign(
-            { "username": foundUser.username },
+            {
+                "Userinfo": { 
+                    "username": foundUser.username,
+                    "roles": roles
+                }
+            },
             process.env.ACCESS_TOKEN_SECRET,
             { expiresIn: '30s' }
         );
