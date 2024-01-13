4005ea49f0bcf37aa89c883e0bf3ffbff3ee302d
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-13 01:00:19 +0100

    doing what i love to do i think im loving backed more

--- lesson4-webserver/middleware/verifyJWT.js
+++ lesson4-webserver/middleware/verifyJWT.js
@@ -1,17 +1,18 @@
+const { request } = require('express');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const verifyJWT = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    if (!authHeader) return res.sendStatus(401);
-    console.log(authHeader); // Bearer token
+    const authHeader = req.headers.authorization || request.header.Authorization;
+    if (!authHeader?.startWith('Bearre')) return res.sendStatus(401);
     const token = authHeader.split(' ')[1];
     jwt.verify(
         token,
         process.env.ACCESS_TOKEN_SECRET,
         (err, decoded) => {
             if (err) return res.sendStatus(403); //invalid token
-            req.user = decoded.username;
+            req.user = decoded.Userinfo.username;
+            req.roles = decoded.Userinfo.roles;
             next();
         }
     );
