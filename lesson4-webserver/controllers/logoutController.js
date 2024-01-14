2579935ab4dc38e915d8f4ea04b2e5416d72a96d
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-15 00:30:14 +0100

    doing what i love to do i think im loving backed more

--- lesson4-webserver/controllers/logoutController.js
+++ lesson4-webserver/controllers/logoutController.js
@@ -8,20 +8,16 @@ const handleLogout = async (req, res) => {
     const refreshToken = cookies.jwt;
 
     // Is refreshToken in db?
-    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+    const foundUser = await User.findOne({ refreshToken }).exec();
     if (!foundUser) {
         res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
         return res.sendStatus(204);
     }
 
     // Delete refreshToken in db
-    const otherUsers = usersDB.users.filter(person => person.refreshToken !== foundUser.refreshToken);
-    const currentUser = { ...foundUser, refreshToken: '' };
-    usersDB.setUsers([...otherUsers, currentUser]);
-    await fsPromises.writeFile(
-        path.join(__dirname, '..', 'model', 'users.json'),
-        JSON.stringify(usersDB.users)
-    );
+   foundUser.refreshToken = '';
+   const result = await foundUser.save();
+   console.log(result)
 
     res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
     res.sendStatus(204);
