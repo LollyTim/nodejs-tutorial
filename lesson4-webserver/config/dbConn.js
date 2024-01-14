cc6296b1e15981e28b900f2b14cb10771d280616
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-01-14 21:46:13 +0100

    another day writing son nodejs and express js code intresting

--- lesson4-webserver/config/dbConn.js
+++ lesson4-webserver/config/dbConn.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-const connectDB = async () => {
-    try{
-        await mongoose.connect(process.env.DATABASE_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        });
+// const connectDB = async () => {
+//     try{
+//         await mongoose.connect(process.env.DATABASE_URI, {
+//             useUnifiedTopology: true,
+//             useNewUrlParser: true
+//         });
 
-    }catch (err){
-        console.error(err);
-    }
-}
+//     }catch (err){
+//         console.error(err);
+//     }
+// }
 
-module.exports = connectDB
+// module.exports = connectDB
 
